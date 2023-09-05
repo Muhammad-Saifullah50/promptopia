1a23c1d1dc43662909e066eb3b5454a9961b9d33
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-09-05 12:35:23 +0500

    started navbar

---
+++ src/components/index.ts
@@ -0,0 +1,15 @@
+import Feed from "./feed/Feed";
+import Form from "./form/Form";
+import Profile from "./profile/Profile";
+import Navbar from "./navbar/Navbar";
+import PromptCard from "./promptCard/PromptCard";
+import Provider from "./provider/Provider";
+
+export {
+    Feed,
+    Form,
+    Profile,
+    Navbar,
+    PromptCard,
+    Provider
+}
