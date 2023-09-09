9e202cfa8a47e0afc6e586d8eaf663709fde5352
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-09-09 12:59:12 +0500

    fixed error

--- src/app/api/prompt/route.ts
+++ src/app/api/prompt/route.ts
@@ -10,7 +10,7 @@ export const GET = async (request: NextRequest) => {
 
         return new Response(JSON.stringify(prompts), { status: 200 })
     } catch (error) {
-        return new Response("Failed to fetch prompts", { status: 200 })
+        return new Response("Failed to fetch prompts", { status: 500 })
 
     }
 }
