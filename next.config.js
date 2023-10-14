0fce9073710910b3c57ace47499734ae5de1cb23
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-10-14 12:17:08 +0500

    modified nextconfig

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+experimental: {
+serverComponentsExternalPackages: ["mongoose"]
+}
     images: {
         domains: ['lh3.googleusercontent.com']
 
--- src/app/api/prompt/route.ts
+++ src/app/api/prompt/route.ts
@@ -1,4 +1,3 @@
-import { NextRequest, NextResponse } from "next/server"
 import { connectToDB } from "@/utils/database"
 import Prompt from "@/models/prompt/prompt"
 
@@ -8,7 +7,7 @@ export const GET = async () => {
 
         const prompts = await Prompt?.find({})?.populate('creator')
 
-        return new Response(JSON.stringify(prompts), { status: 200 })
+      return new Response(JSON.stringify(prompts), { status: 200 })
     } catch (error) {
         console.error(error)
         return new Response(JSON.stringify("Failed to fetch prompts"), { status: 500 })
