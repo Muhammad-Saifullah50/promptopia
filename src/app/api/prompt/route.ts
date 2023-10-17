3c8c22318dadce216cb8aba1224d267d89bead31
Muhammad-Saifullah50 <saifullahm2005@gmail.com>
2023-10-17 21:30:42 +0500

    fixed prompt route

--- src/app/api/prompt/route.ts
+++ src/app/api/prompt/route.ts
@@ -6,9 +6,9 @@ export const GET = async () => {
     try {
         await connectToDB();
 
-        const prompts = await Prompt?.find({})?.populate('creator', model: User)
+        const prompts = await Prompt?.find({})?.populate('creator', { model: User })
 
-      return new Response(JSON.stringify(prompts), { status: 200 })
+        return new Response(JSON.stringify(prompts), { status: 200 })
     } catch (error) {
         console.error(error)
         return new Response(JSON.stringify("Failed to fetch prompts"), { status: 500 })
